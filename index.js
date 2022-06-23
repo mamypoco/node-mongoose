84bb0dcd202755d8d61b680b4c742e6d105817ee
Mami Nishiwaki <mamypoco@gmail.com>
2022-06-23 11:23:34 -0700

    Mongoose Part 3

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//connection to DB server
 const mongoose = require("mongoose");
 const Campsite = require("./models/campsite");
 
 const url = "mongodb://localhost:27017/nucampsite";
 const connect = mongoose.connect(url, {
    useCreateIndex: true,
+   useFindAndModify: false,
    useNewUrlParser: true,
    useUnifiedTopology: true,
 });
@@ -18,10 +20,31 @@ connect.then(() => {
 
       .then((campsite) => {
          console.log(campsite);
-         return Campsite.find();
+
+         return Campsite.findByIdAndUpdate(
+            campsite._id,
+            {
+               $set: { description: "Updated Test Document" },
+            },
+            {
+               new: true,
+               //return the updated document, otherwise it will return the original document before it was updated.
+            }
+         );
       })
-      .then((campsites) => {
-         console.log(campsites);
+      .then((campsite) => {
+         console.log(campsite);
+
+         campsite.comments.push({
+            rating: 5,
+            text: "What a magnificent view!",
+            author: "Tinus Lorvaldes",
+         });
+
+         return campsite.save();
+      })
+      .then((campsite) => {
+         console.log(campsite);
          return Campsite.deleteMany();
          //The deleteMany() function is used to delete all of the documents that match conditions from the collection. This function behaves like the remove() function but it deletes all documents that match conditions regardless of the single option.
       })
--- models/campsite.js
+++ models/campsite.js
@@ -1,5 +1,28 @@
+//Setup Schema for campsite documents
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+   {
+      rating: {
+         type: Number,
+         min: 1,
+         max: 5,
+         required: true,
+      },
+      text: {
+         type: String,
+         required: true,
+      },
+      author: {
+         type: String,
+         required: true,
+      },
+   },
+   {
+      timestamps: true,
+   }
+);
 
 const campsiteSchema = new Schema(
    {
@@ -12,6 +35,8 @@ const campsiteSchema = new Schema(
          type: String,
          required: true,
       },
+      comments: [commentSchema],
+      //this is to insert sub-document. You can have multiple sub-documents.
    },
    {
       timestamps: true,
